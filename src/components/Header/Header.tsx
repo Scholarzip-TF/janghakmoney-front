274b6019eafbc5486e2b0c6ccde86eb2ceb85ae2
shwn <zxz8673@gmail.com>
2025-02-09 15:34:02 +0900

    feat: 헤더 url / 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,10 +4,12 @@ import './Header.css';
 export const Header = () => {
   return (
     <header className="header">
-      <div className="header-content">
-        <img src={logo} alt="로고" className="logo" />
+      <a href="/" className="logo-link">
+        <div className="header-content">
+          <img src={logo} alt="로고" className="logo" />
         <h1>내 장학금 알아보기</h1>
-      </div>
+        </div>
+      </a>
     </header>
   );
 };
