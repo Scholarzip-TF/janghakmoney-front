3edebbda6d38367f0892f7d7e9070420973a1618
shwn <zxz8673@gmail.com>
2025-02-09 15:36:48 +0900

    fix: 헤더 밑줄 삭제

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
       <a href="/" className="logo-link">
         <div className="header-content">
           <img src={logo} alt="로고" className="logo" />
-        <h1>내 장학금 알아보기</h1>
+          <h1>내 장학금 알아보기</h1>
         </div>
       </a>
     </header>
